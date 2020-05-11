43cff9df7ee7b83d941d4eb1c44cc790b2c6a4ca
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-05-11 16:54:40 -0500

    entidad jobs

--- src/main/java/com/example/demo/Entity/Jobs.java
+++ src/main/java/com/example/demo/Entity/Jobs.java
@@ -1,7 +1,28 @@
 package com.example.demo.Entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "jobs")
 public class Jobs {
-    private int job_id;
+    @Id
+    @OneToMany
+    @JoinColumn(name = "job_id")
+    @Digits(integer = 10,fraction = 0)
+    @Positive
+    private Employees job_id;
+    @Column(nullable = false)
+    @NotBlank
+    @Size(max=40)
     private String job_title;
-    private
+    @Digits(integer = 6, fraction = 0)
+    @Positive
+    private int min_salary;
+    @Digits(integer = 6, fraction = 0)
+    @Positive
+    private int max_salary;
 }
